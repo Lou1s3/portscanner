8a551b8957cf2adbed8fcf143f299f805bcf57c2
Emmanuelle Louise <112348634+Lou1s3@users.noreply.github.com>
2022-08-28 17:50:18 -0300

    Update port-scanner.py

--- port-scanner.py
+++ port-scanner.py
@@ -9,6 +9,7 @@
 import socket
 import threading
 import concurrent.futures
+import argparse
 
 print_lock = threading.Lock()
 
@@ -29,9 +30,11 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
     for port in range(1000):
         executor.submit(scan, ip, port +1)
 
-def get_arguments():
+def get_arguments(): 
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--ports", action="store",  help="Irá indicar a quantidade de portas para serem escaneadar.")
     parser.add_argument("-t", "--ip", action="store", help="O target é o endereço de IPV4. Caso não exista um IPV4, não terá como identificar se a porta está aberta.")
     parser.add_argument("-o", "--lock", action="store", help="Vai fazer com que não seja indicada mais de 1 porta ao mesmo tempo, fazendo com que não embaralhe.)")
     args = parser.parse_args()
+    
+    return args
